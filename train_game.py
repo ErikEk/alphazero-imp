f7877391465aa0c7a1c1ceea1b9ba0bcb2dd15d3
ErikEk <erik.ek@gmail.com>
2025-03-20 23:07:49 +0100

    s

--- train_game.py
+++ train_game.py
@@ -82,6 +82,9 @@ class ConnectFour:
         encoded_state = np.stack(
             (state == -1, state == 0, state == 1)
         ).astype(np.float32)
+        
+        if len(state.shape) == 3:
+            encoded_state = np.swapaxes(encoded_state, 0, 1)
 
         return encoded_state
 
@@ -139,6 +142,10 @@ class TickTacToe:
         encoded_state = np.stack(
             (state == -1, state == 0, state == 1)
         ).astype(np.float32)
+
+        if len(state.shape) == 3:
+            encoded_state = np.swapaxes(encoded_state, 0, 1)
+
         return encoded_state
 
 class Node:
@@ -255,6 +262,109 @@ class ResBlock(nn.Module):
 
         return x
 
+
+class AlphaZeroParallel:
+    ''' AlphaZeroParallel '''
+    def __init__(self, model, optimizer, game, args):
+        self.model = model
+        self.optimizer = optimizer
+        self.game = game
+        self.args = args
+        self.mcts = MCTSParallel(game, args, model)
+
+    def selfPlay(self):
+        return_memory = []
+        player = 1
+        spGames = [SPG(self.game) for spg in range(self.args['num_parallel_games'])]
+
+        while len(spGames) > 0:
+            states = np.stack([spg.state for spg in spGames])
+
+            neutral_states = self.game.change_perspective(states, player)
+
+            self.mcts.search(neutral_states, spGames)
+
+            for i in range(len(spGames))[::-1]:
+                spg = spGames[i]
+                action_probs = np.zeros(self.game.action_size)
+                for child in spg.root.children:
+                    action_probs[child.action_taken] = child.visit_count
+                action_probs /= np.sum(action_probs)
+            
+                spg.memory.append((spg.root.state, action_probs, player))
+
+                temperature_action_probs = action_probs ** (1 / self.args['temperature'])
+                action = np.random.choice(self.game.action_size, p=temperature_action_probs) # change to p=action_probs
+
+                spg.state = self.game.get_next_state(spg.state, action, player)
+
+                value, is_terminal = self.game.get_value_and_terminated(spg.state, action)
+
+                if is_terminal:
+                    for hist_neutral_state, hist_action_probs, hist_player in spg.memory:
+                        hist_outcome = value if hist_player == player else self.game.get_opponent_value(value)
+                        return_memory.append((
+                            self.game.get_encoded_state(hist_neutral_state),
+                            hist_action_probs,
+                            hist_outcome
+                        ))
+
+                    del spGames[i]
+
+            player = self.game.get_opponent(player)
+        return return_memory
+    
+    def train(self, memory):
+        random.shuffle(memory)
+        for batchIdx in range(0, len(memory), self.args['batch_size']):
+            sample = memory[batchIdx:min(len(memory) - 1, batchIdx + self.args['batch_size'])] # Change to memory[batchIdx:batchIdx+self.args['batch_size']] in case of an error
+            state, policy_targets, value_targets = zip(*sample)
+
+            state, policy_targets, value_targets = np.array(state), np.array(policy_targets), np.array(value_targets).reshape(-1, 1)
+
+            state = torch.tensor(state, dtype=torch.float32, device=self.model.device)
+            policy_targets = torch.tensor(policy_targets, dtype=torch.float32, device=self.model.device)
+            value_targets = torch.tensor(value_targets, dtype=torch.float32, device=self.model.device)
+
+            out_policy, out_value = self.model(state)
+
+            policy_loss = F.cross_entropy(out_policy, policy_targets)
+            value_loss = F.mse_loss(out_value, value_targets)
+            loss = policy_loss + value_loss
+
+            self.optimizer.zero_grad() # change to self.optimizer
+            loss.backward()
+            self.optimizer.step() # change to self.optimizer
+
+    def learn(self):
+        for iteration in range(self.args['num_iterations']):
+            memory = []
+
+            self.model.eval()
+            for selfPlay_iteration in range(self.args['num_selfPlay_iterations'] // self.args['num_parallel_games']):
+                memory += self.selfPlay()
+                if selfPlay_iteration % 100 == 0:
+                    print(f"iteration: {selfPlay_iteration}")
+            self.model.train()
+            for epoch in range(self.args['num_epochs']):
+                self.train(memory)
+                print(f"epoch: {epoch}")
+
+            if os.path.exists(f"models/model_{iteration}_{self.game}.pt"):  # Check if file exists
+                os.remove(f"models/model_{iteration}_{self.game}.pt")
+            torch.save(self.model.state_dict(), f"models/model_{iteration}_{self.game}.pt")
+            if os.path.exists(f"models/optimizer_{iteration}.pt"):  # Check if file exists
+                os.remove(f"models/optimizer_{iteration}_{self.game}.pt")
+            torch.save(self.optimizer.state_dict(), f"models/optimizer_{iteration}_{self.game}.pt")
+
+class SPG:
+    ''' SPG '''
+    def __init__(self, game):
+        self.state = game.get_initial_state()
+        self.memory = []
+        self.root = None
+        self.node = None
+
 class AlphaZero:
     ''' AlphaZero '''
     def __init__(self, model, optimizer, game, args):
@@ -276,7 +386,7 @@ class AlphaZero:
             memory.append((neutral_state, action_probs, player))
 
             temperature_action_probs = action_probs ** (1 / self.args['temperature'])
-            action = np.random.choice(self.game.action_size, p=action_probs) # change to p=temperature_action_probs
+            action = np.random.choice(self.game.action_size, p=temperature_action_probs) # change to p=action_probs
 
             state = self.game.get_next_state(state, action, player)
 
@@ -331,12 +441,81 @@ class AlphaZero:
                 self.train(memory)
                 print(f"epoch: {epoch}")
 
-            if os.path.exists(f"models/model_{iteration}.pt"):  # Check if file exists
-                os.remove(f"models/model_{iteration}.pt")
-            torch.save(self.model.state_dict(), f"models/model_{iteration}.pt")
+            if os.path.exists(f"models/model_{iteration}_{self.game}.pt"):  # Check if file exists
+                os.remove(f"models/model_{iteration}_{self.game}.pt")
+            torch.save(self.model.state_dict(), f"models/model_{iteration}_{self.game}.pt")
             if os.path.exists(f"models/optimizer_{iteration}.pt"):  # Check if file exists
-                os.remove(f"models/optimizer_{iteration}.pt")
-            torch.save(self.optimizer.state_dict(), f"models/optimizer_{iteration}.pt")
+                os.remove(f"models/optimizer_{iteration}_{self.game}.pt")
+            torch.save(self.optimizer.state_dict(), f"models/optimizer_{iteration}_{self.game}.pt")
+
+
+class MCTSParallel:
+    ''' MCTSParallel '''
+    def __init__(self, game, args, model):
+        self.game = game
+        self.args = args
+        self.model = model
+        
+    @torch.no_grad()
+    def search(self, states, spGames):
+        
+        
+        policy, _ = self.model(
+            torch.tensor(self.game.get_encoded_state(states), device=self.model.device)
+        )
+        policy = torch.softmax(policy, axis=1).cpu().numpy()
+        policy = (1 - self.args['dirichlet_epsilon']) * policy + self.args['dirichlet_epsilon'] \
+            * np.random.dirichlet([self.args['dirichlet_alpha']] * self.game.action_size, size=policy.shape[0])
+        
+
+        for i, spg in enumerate(spGames):
+            spg_policy = policy[i]
+            valid_moves = self.game.get_valid_moves(states[i])
+            spg_policy *= valid_moves
+            spg_policy /= np.sum(spg_policy)
+
+            spg.root = Node(self.game, self.args, states[i], visit_count=1)
+            spg.root.expand(spg_policy)
+        
+        for search in range(self.args['num_searches']):
+            for spg in spGames:
+                spg.node = None
+                node = spg.root
+                
+                while node.is_fully_expanded():
+                    node = node.select()
+                    
+                value, is_terminal = self.game.get_value_and_terminated(node.state, node.action_taken)
+                value = self.game.get_opponent_value(value)
+                
+
+                if is_terminal:
+                    node.backpropagate(value)
+                else:
+                    spg.node = node
+
+            expandable_spGames = [mappingIdx for mappingIdx in range(len(spGames)) if spGames[mappingIdx].node is not None]
+
+            if len(expandable_spGames) > 0:
+                states = np.stack([spGames[mappingIdx].node.state for mappingIdx in expandable_spGames])
+
+                policy, value = self.model(
+                    torch.tensor(self.game.get_encoded_state(states), device=self.model.device)
+                )
+                policy = torch.softmax(policy, axis=1).cpu().numpy()
+
+            for i, mappingIdx in enumerate(expandable_spGames):
+                node = spGames[mappingIdx].node
+                spg_policy, spg_value = policy[i], value[i]
+
+                valid_moves = self.game.get_valid_moves(node.state)
+                spg_policy *= valid_moves
+                spg_policy /= np.sum(spg_policy)
+
+                node.expand(spg_policy)
+                node.backpropagate(spg_value)
+
+
 
 class MCTS:
     ''' MCTS '''
@@ -452,22 +631,22 @@ exit(0)
 '''
 
 # TRAIN CONNECTFOUR
-'''
 def main():
     game = ConnectFour()
     print(torch.cuda.get_device_name(0))
     print(torch.version.cuda)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"training on {device}")
-    model = ResNet(game, 4, 64, device)
+    model = ResNet(game, 9, 128, device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
     args = {
         'C': 2,
-        'num_searches': 60,
-        'num_iterations': 3,
+        'num_searches': 600,
+        'num_iterations': 8,
         'num_selfPlay_iterations': 500,
+        'num_parallel_games': 100,
         'num_epochs': 4,
-        'batch_size': 64,
+        'batch_size': 128,
         'temperature': 1.25,
         'dirichlet_epsilon': 0.25,
         'dirichlet_alpha': 0.3
@@ -475,10 +654,10 @@ def main():
 
     os.makedirs("models", exist_ok=True)
 
-    alphaZero = AlphaZero(model, optimizer, game, args)
+    alphaZero = AlphaZeroParallel(model, optimizer, game, args)
     alphaZero.learn()
     exit(0)
-'''
+
 
 # TRAIN TICTACKTOE
 '''def main():
@@ -508,9 +687,11 @@ def main():
     exit(0)
 '''
 
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
 
+# VALIDATION CODE
+'''
 player = 1
 
 args = {
@@ -559,3 +740,4 @@ while True:
         break
 
     player = game.get_opponent(player)
+'''
